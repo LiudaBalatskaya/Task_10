322a3afbf83458d3af53924ec78d01584400ec6c
LiudaB <LiudaBalatskaya@coherentsolutions.com>
2017-06-07 15:28:29 +0300

    After correcting

--- src/test/java/MethodSortedTest.java
+++ src/test/java/MethodSortedTest.java
@@ -6,43 +6,41 @@ import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by LiudaBalatskaya on 5/5/2017.
- */
+
 @Category(FastTests.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class TestMethodOrder {
+public class MethodSortedTest {
     public int result;
     public Mathematics calculator;
 
     @Before
-    public void initializerTest(){
+    public void initializer(){
         calculator = new Mathematics();
         calculator.setResult(0);
     }
 
     @Test
-    public void testA(){
+    public void multiply(){
         calculator.multiply(5,5);
         result = calculator.getResult();
         assertEquals(25,result);
     }
 
     @Test
-    public void testB(){
+    public void divide(){
         calculator.divide(25,5);
         result = calculator.getResult();
         assertEquals(5,result);
     }
 
     @Test
-    public void testC(){
+    public void add(){
         calculator.add(2,2);
         result = calculator.getResult();
         assertEquals(4,result);
     }
     @Test
-    public void testD(){
+    public void deduct(){
         calculator.setResult(1);
         calculator.deduct(15,5);
         result = calculator.getResult();
